29f5e7521f267c470645eef8472843d2a6ae21d0
EugeneKyoto <eugenebubnov@vk.com>
2022-12-08 13:42:34 +0300

    Операторы и операнды

---
+++ Part One/Ch.1/2.8 - 2.9 Numbers.py
@@ -0,0 +1,11 @@
+# 2.8 Число 8.
+a = 3
+b = 5
+print(a + b)
+print(40 - 32)
+print(48 // 6)
+print(4 * 2, "", end='\n') # 
+
+# 2.9 Любимое число.
+favorive_num = 16
+print(f'My favorite number is {favorive_num}!')
