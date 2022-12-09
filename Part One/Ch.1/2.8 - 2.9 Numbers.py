0508ee64913a3afb2d308baa070f0c5c00715dad
EugeneKyoto <eugenebubnov@vk.com>
2022-12-09 11:09:05 +0300

    Списки, индексация списков

--- Part One/Ch.1/2.8 - 2.9 Numbers.py
+++ Part One/Ch.1/2.8 - 2.9 Numbers.py
@@ -4,8 +4,8 @@ b = 5
 print(a + b)
 print(40 - 32)
 print(48 // 6)
-print(4 * 2, "", end='\n') # 
+print(4 * 2, "", sep='\n') #Разделение двух заданий, при помощи пустой строки.
 
 # 2.9 Любимое число.
 favorive_num = 16
-print(f'My favorite number is {favorive_num}!')
+print(f'My favorite number is {favorive_num}!') #f-строки топ! :)
---
+++ Part One/Ch.1/3.1 - 3.3 Lists.py
@@ -0,0 +1,13 @@
+# 3.1 Имена.
+names = ['Anton', 'Pavel', 'Alex', 'Max']
+print(names[0], names[1], names[2], names[3], sep='\n')
+print()
+
+# 3.2 Сообщения.
+for name in names: #Выполнил через цикл, чтобы не писать больше кода.
+    print(f'Hello {name}, go to Fortnite! :)')
+print()
+
+# 3.3 Собственный список.
+cars = ['Vortex', 'Bmw', 'Mitsubishi', 'Audi', 'Lada', 'Mercedes']
+print(f'Я хотел бы купить {cars[2]}!')
