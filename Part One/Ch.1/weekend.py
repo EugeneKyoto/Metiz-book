67277a3dce8f8b54c669102eda02ece369281b53
EugeneKyoto <eugenebubnov@vk.com>
2022-12-11 17:25:36 +0300

    Job

--- Part One/Ch.1/weekend.py
+++ Part One/Ch.1/weekend.py
@@ -2,3 +2,8 @@ for i in range(10):
     print('Выходные!')
     if i == 5:
         print('УРА!')
+#11.12.2022
+# Делаю тестовое задание, хочу работать в техподдержке.
+name = 'Eugene'
+job = 'Sweet Dreams'
+print(name, job, 'Waiting for.')
